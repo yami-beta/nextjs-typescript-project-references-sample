5ce2625cb8d44d1ecbbb14035b825d99b2a38673
yami-beta <yami-beta@users.noreply.github.com>
2020-10-26 14:19:40 +0900

    feat: Hello World

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true
+  },
+  plugins: ["@typescript-eslint", "react-hooks"],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  rules: {
+    // interface や jsx をうまく扱えないので off にして noUnusedLocals を tsconfig で有効化
+    "no-unused-vars": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: "module"
+  }
+};
